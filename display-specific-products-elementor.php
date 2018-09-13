30152089a1f7c886df2a426a9b8140b2cbad1d82
udidol <36724484+udidol@users.noreply.github.com>
2018-09-13 14:45:32 +0300

    Initial Commit
    Initial Commit

---
+++ display-specific-products-elementor.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * Plugin Name: Display Specific Products for Elementor
+ * Description: Adds a widget to the Elementor Page Builder plugin, which displays a selected WooCommerce product's featured image and title.
+ * Author: Udi Dollberg
+ * Version: 1.0.0
+ *
+ * Text Domain: udi_dsc
+ *
+ * The Display Specific Products for Elementor plugin is free software: 
+ * you can redistribute it and/or modifyit under the terms of the GNU 
+ * General Public License as published bythe Free Software Foundation, 
+ * either version 3 of the License, or any later version.
+ *
+ * The Display Specific Products for Elementor plugin is distributed in 
+ * the hope that it will be useful,but WITHOUT ANY WARRANTY; without even 
+ * the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR 
+ * PURPOSE. See the GNU General Public License for more details.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Create constants for file path references
+define( 'UDI_DSPE__FILE__', __FILE__ );
+define( 'UDI_DSPE_PLUGIN_BASE', plugin_basename( UDI_DSPE__FILE__ ) );
+define( 'UDI_DSPE_PATH', plugin_dir_path( UDI_DSPE__FILE__ ) );
+define( 'UDI_DSPE_URL', plugins_url( '/', UDI_DSPE__FILE__ ) );
+
+$dspeProductsArray = array();
+
+// Create an array of products with IDs as Keys and Titles as Values, then sort it alphabetically
+function cspa($dspeProductsArray) {
+
+	global $dspeProductsArray;
+
+    $args = array(
+        'post_type' => 'product',
+        'posts_per_page' => -1
+        );
+    $loop = new WP_Query( $args );
+    if ( $loop->have_posts() ) {
+		while ( $loop->have_posts() ) : $loop->the_post();
+			$dspePostID = (string) get_the_ID();
+			$dspePostTitle = get_the_title();
+			$dspeProductsArray[$dspePostID] = $dspePostTitle;
+        endwhile;
+    } else {
+        echo __( 'No products found' );
+    }
+
+	ksort($dspeProductsArray);
+
+    wp_reset_postdata();
+}
+
+function udi_dspe_load_plugin() {
+	// Check if Elementor is loaded before loading the plugin. If it isn't loaded, display admin notice.
+	if ( ! did_action( 'elementor/loaded' ) ) {
+		add_action( 'admin_notices', 'udi_dspe_fail_load' );
+		return;
+	}
+	// Check if the installed version of Elementor matches the minimum required version for the widget to work
+	$elementor_version_required = '1.8.0';
+	if ( ! version_compare( ELEMENTOR_VERSION, $elementor_version_required, '>=' ) ) {
+		add_action( 'admin_notices', 'udi_dspe_fail_load_out_of_date' );
+		return;
+	}
+	// Call the file with the actual widget
+	DisplaySpecificProductsElementor::get_instance()->init();
+	// Load Plugin CSS to display in Elementor
+	add_action('wp_head', 'add_dspe_css');
+}
+add_action( 'plugins_loaded', 'udi_dspe_load_plugin' );
+
+// This should run if Elementor fails to load when we try to load the plugin
+function udi_dspe_fail_load() {
+	$screen = get_current_screen();
+	if ( isset( $screen->parent_file ) && 'plugins.php' === $screen->parent_file && 'update' === $screen->id ) {
+		return;
+	}
+	$plugin = 'elementor/elementor.php';
+	if ( _is_elementor_installed() ) {
+		if ( ! current_user_can( 'activate_plugins' ) ) {
+			return;
+		}
+		$activation_url = wp_nonce_url( 'plugins.php?action=activate&amp;plugin=' . $plugin . '&amp;plugin_status=all&amp;paged=1&amp;s', 'activate-plugin_' . $plugin );
+		$message = '<p>' . __( 'Display Specific Products for Elementor is not working because you need to activate the Elementor plugin.', 'udi-dspe' ) . '</p>';
+		$message .= '<p>' . sprintf( '<a href="%s" class="button-primary">%s</a>', $activation_url, __( 'Activate Elementor Now', 'udi-dspe' ) ) . '</p>';
+	} else {
+		if ( ! current_user_can( 'install_plugins' ) ) {
+			return;
+		}
+		$install_url = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=elementor' ), 'install-plugin_elementor' );
+		$message = '<p>' . __( 'Display Specific Products for Elementor is not working because you need to install the Elementor plugin', 'udi-dspe' ) . '</p>';
+		$message .= '<p>' . sprintf( '<a href="%s" class="button-primary">%s</a>', $install_url, __( 'Install Elementor Now', 'udi-dspe' ) ) . '</p>';
+	}
+	echo '<div class="error"><p>' . $message . '</p></div>';
+}
+
+// This should run if Elementor's version is too old for the plugin to run
+function elementor_pro_fail_load_out_of_date() {
+	if ( ! current_user_can( 'update_plugins' ) ) {
+		return;
+	}
+	$file_path = 'elementor/elementor.php';
+	$upgrade_link = wp_nonce_url( self_admin_url( 'update.php?action=upgrade-plugin&plugin=' ) . $file_path, 'upgrade-plugin_' . $file_path );
+	$message = '<p>' . __( 'Display Specific Products for Elementor is not working because you are using an old version of Elementor.', 'udi-dspe' ) . '</p>';
+	$message .= '<p>' . sprintf( '<a href="%s" class="button-primary">%s</a>', $upgrade_link, __( 'Update Elementor Now', 'udi-dspe' ) ) . '</p>';
+	echo '<div class="error">' . $message . '</div>';
+}
+
+if ( ! function_exists( '_is_elementor_installed' ) ) {
+	function _is_elementor_installed() {
+		$file_path = 'elementor/elementor.php';
+		$installed_plugins = get_plugins();
+		return isset( $installed_plugins[ $file_path ] );
+	}
+}
+
+class DisplaySpecificProductsElementor {
+
+	private static $instance = null;
+ 
+	public static function get_instance() {
+	   if ( ! self::$instance )
+		  self::$instance = new self;
+	   return self::$instance;
+	}
+ 
+	public function init(){
+	   add_action( 'elementor/widgets/widgets_registered', array( $this, 'widgets_registered' ) );
+	   add_action( 'elementor/widgets/widgets_registered', 'cspa' );
+	}
+ 
+	public function widgets_registered() {
+ 
+	   // We check if the Elementor plugin has been installed / activated.
+	   if(defined('ELEMENTOR_PATH') && class_exists('Elementor\Widget_Base')){
+ 
+		  // We look for any theme overrides for this custom Elementor element.
+		  // If no theme overrides are found we use the default one in this plugin.
+ 
+		  $widget_file = 'plugins/elementor/dspe.php';
+		  $template_file = locate_template($widget_file);
+		  if ( !$template_file || !is_readable( $template_file ) ) {
+			 $template_file = plugin_dir_path(__FILE__).'dspe.php';
+		  }
+		  if ( $template_file && is_readable( $template_file ) ) {
+			 require_once $template_file;
+		  }
+	   }
+	}
+ }
+
+  // When invoked, adds plugin CSS to head
+  function add_dspe_css() {
+
+	?>
+
+	<style>
+		.elementor-widget-udi_dsc .elementor-widget-container {
+			display: flex;
+			flex-flow: row wrap;
+		}
+		.udi-dsc-container {
+			display: inline-block;
+			/* flex: 1; */
+			vertical-align: top;
+		}
+		.udi-dsc-product-image {
+			text-align: center;
+		}
+		.udi-dsc-product-title {
+			text-align: center;
+		}
+
+		.udi-dsc-product-title a {
+			font-weight: bold;
+		}
+	</style>
+
+	 <?php
+}
---
+++ dspe.php
@@ -0,0 +1,259 @@
+<?php
+namespace Elementor;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+// Notice if the Elementor is not active
+if ( ! did_action( 'elementor/loaded' ) ) {
+	// add_action( 'admin_notices', 'udi_dsc_fail_load' );
+	return;
+}
+
+
+class Udi_DSPE_Widget extends \Elementor\Widget_Base {
+
+	public function get_name() {
+		return 'udi_dsc';
+	}
+
+	public function get_title() {
+		return 'Display Specific Products';
+	}
+
+	public function get_icon() {
+		return 'eicon-posts-grid';
+	}
+
+	public function get_categories() {
+		return [ 'general' ];
+	}
+
+	protected function _register_controls() {
+
+        global $dspeProductsArray;
+
+        $this->start_controls_section(
+			'content_section',
+			[
+				'label' => __( 'Content', 'udi_dsc' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+			]
+		);
+            
+            $this->add_control(
+                'products_list',
+                [
+                    'label' => __( 'Choose Products', 'udi_dsc' ),
+                    'type' => \Elementor\Controls_Manager::SELECT2,
+                    'multiple' => true,
+                    'options' => $dspeProductsArray,
+                    'section' => 'content_section',
+                    'default' => [ '' ],
+                ]
+            );
+
+            $this->add_control(
+                'products_per_row',
+                [
+                    'label' => __( 'Products Per Row', 'udi_dsc' ),
+                    'type' => \Elementor\Controls_Manager::SELECT,
+                    'default' => '1',
+                    'options' => [
+                        '1' => 1,
+                        '2' => 2,
+                        '3' => 3,
+                        '4' => 4,
+                    ],
+                ]
+            );
+
+            $this->add_control(
+                'randomize',
+                [
+                    'label' => __( 'Randomize Product Order', 'udi_dsc' ),
+                    'type' => \Elementor\Controls_Manager::SWITCHER,
+                    'label_on' => __( 'On', 'udi_dsc' ),
+                    'label_off' => __( 'Off', 'udi_dsc' ),
+                    'return_value' => 'yes',
+                    'default' => 'yes',
+                ]
+            );
+
+        $this->end_controls_section();
+
+	}
+
+	protected function render( $instance = [] ) {
+        
+        global $dspeProductsArray;
+
+		$settings = $this->get_settings_for_display();
+    
+        // Get number of products per row from user's chosen widget settings, and make it into a global var.
+        $GLOBALS['products_per_row'] = !empty($settings['products_per_row']) ? intval($settings['products_per_row']) : 1;
+        $products_per_row = $GLOBALS['products_per_row'];
+
+        if ( 'yes' === $settings['randomize'] ) {
+			shuffle($settings['products_list']);
+		}
+
+        ?>
+
+        <style>
+        <?php
+        if ( !empty($settings['products_list']) && ( count($settings['products_list']) % $products_per_row ) === 0 ) : ?>
+            .elementor-widget-udi_dsc .elementor-widget-container {
+                justify-content: space-between;
+		    }
+        <?php endif;
+
+        if ( !empty($settings['products_list']) && ( count($settings['products_list']) % $products_per_row ) !== 0 ) : ?>
+            
+            .udi-dsc-container {
+                margin-right: 1%;
+            }
+
+            .elementor-widget-udi_dsc .elementor-widget-container {
+                justify-content: flex-start;
+		    }
+            
+            <?php if ( $products_per_row == 2 ) : ?>
+                .udi-dsc-container {
+                    margin-right: 2%;
+                }
+            <?php endif; ?>
+
+            <?php if ( $products_per_row == 3 ) : ?>
+                .udi-dsc-container {
+                    margin-right: 1.5%;
+                }
+            <?php endif; ?>
+
+            <?php if ( $products_per_row == 4 ) : ?>
+                .udi-dsc-container {
+                    margin-right: 1.33%;
+                }
+            <?php endif; ?>
+
+            .udi-dsc-container:nth-of-type(<?php echo $products_per_row; ?>n+<?php echo $products_per_row; ?>) {
+                margin-right: 0;
+            }
+        <?php endif; ?>
+
+        .udi-dsc-container {
+            width: <?php echo ((100 / $products_per_row)-1); ?>%;
+            margin-bottom: 1em;
+        }
+        
+        <?php if ($products_per_row > 1): ?>
+            @media only screen and (max-width: 800px) {
+                .udi-dsc-container {
+                    width: 49%;
+                }
+
+                .udi-dsc-container:nth-of-type(<?php echo $products_per_row; ?>n+<?php echo $products_per_row; ?>) {
+                    margin-right: 2%;
+                }
+
+                .udi-dsc-container:nth-of-type(2n+2) {
+                    margin-right: 0;
+                }
+            }
+
+            @media only screen and (max-width: 480px) {
+                .udi-dsc-container {
+                    width: 100%;
+                }
+
+                .udi-dsc-container:nth-of-type(<?php echo $products_per_row; ?>n+<?php echo $products_per_row; ?>) {
+                    margin-right: 0;
+                }
+
+                .udi-dsc-container:nth-of-type(2n+2) {
+                    margin-right: 0;
+                }
+            }
+        <?php endif; ?>
+        
+        /* zero left padding on first row element */
+        /* .udi-dsc-container:nth-of-type(<?php echo $products_per_row; ?>n+1) {
+			padding-left: 0.5%;
+            padding-right: 0.5%;
+		} */
+        /* half the left padding on second(!) row element */
+        /* .udi-dsc-container:nth-of-type(<?php echo $products_per_row; ?>n+2) {
+			padding-left: 0.5%;
+            padding-right: 0.5%;
+		} */
+        /* half the right padding on second to last(!) row element */
+        /* .udi-dsc-container:nth-of-type(<?php echo $products_per_row; ?>n+<?php echo $products_per_row-1; ?>) {
+            padding-right: 0.5%;
+            padding-left: 0.5%;
+		} */
+        /* zero right padding on last row element */
+        /* .udi-dsc-container:nth-of-type(<?php echo $products_per_row; ?>n+<?php echo $products_per_row; ?>) {
+			padding-right: 0.5%;
+            padding-left: 0.5%;
+		} */
+        </style>
+
+        <?php
+
+        if ( !empty($settings['products_list']) ) {
+            foreach ($settings['products_list'] as $productID) {
+                $product_image = (wp_get_attachment_image_src( get_post_thumbnail_id( intval($productID) ), 'large' ) !== false ) ? wp_get_attachment_image_src( get_post_thumbnail_id( intval($productID) ), 'large' ) : array( esc_url(plugins_url().'/display-specific-products-elementor/img/placeholder.png') );
+                $product_image_url = $product_image[0];
+                ?>
+
+                <div class="udi-dsc-container">
+                    <div class="udi-dsc-product">
+                        <div class="udi-dsc-product-image">
+                            <a href="<?php get_permalink( $productID ) ?>"><img src="<?php echo $product_image_url; ?>"></a>
+                        </div>
+                        <div class="udi-dsc-product-title">
+                            <a href="<?php get_permalink( $productID ) ?>"><?php echo esc_attr( get_the_title($productID) ); ?></a>
+                        </div>
+                    </div><!-- /udi-dsc-product -->
+                </div><!-- /udi-dsc-container -->
+
+                <?php
+            }
+        }
+        else { // If no products are selected
+            ?>
+            
+            <div class="udi-dsc-container">
+                <div class="udi-dsc-product">
+                    <div class="udi-dsc-product-image">
+                        <img src="<?php echo esc_url(plugins_url().'/display-specific-products-elementor/img/placeholder.png'); ?>">
+                    </div>
+                    <div class="udi-dsc-product-title">
+                        <p>Unavailable: No product was chosen</p>
+                    </div>
+                </div><!-- /udi-dsc-product -->
+            </div><!-- /udi-dsc-container -->
+            
+            <?php
+        }
+        
+
+	}
+  
+    protected function content_template() {}
+  
+    public function render_plain_content( $instance = [] ) {}
+
+}
+
+/**
+ * Register Widget
+ *
+ * @since 1.0.0
+ *
+ * @access private
+ */
+\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Udi_DSPE_Widget() );
+//Plugin::instance()->widgets_manager->register_widget( 'Elementor\Udi_DSPE_Widget' );
+
